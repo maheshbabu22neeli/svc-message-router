f9cb6d05986ed0667ad6cd3bfcfbe58905c25f32
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-01-30 20:55:38 +1100

    SN: Initial Commit

---
+++ src/main/java/com/sinch/message/router/SvcMessageRouterApplication.java
@@ -0,0 +1,13 @@
+package com.sinch.message.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcMessageRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcMessageRouterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinch/message/router/SvcMessageRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinch.message.router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcMessageRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
